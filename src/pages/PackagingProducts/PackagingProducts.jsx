cf4ee5d3394c9547cc8881873d1ec97e596abc13
PavitarSharma <pavitarsharma144@gmail.com>
2025-01-28 09:32:48 +0530

    Packaging Solution

--- src/pages/PackagingProducts/PackagingProducts.jsx
+++ src/pages/PackagingProducts/PackagingProducts.jsx
@@ -19,7 +19,7 @@ const PackagingProducts = () => {
               </Link>
             ))}
           </div>
-          <div className="p-4 flex-1">
+          <div className="lg:p-4 py-6 flex-1">
             <Outlet />
           </div>
         </div>
